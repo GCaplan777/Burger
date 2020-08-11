7cd34ef642f626906138a42a39f79fd15fc12dfa
GCaplan <gabriel.caplan@gmail.com>
2020-08-11 11:54:52 -0400

    updated env

--- config/connection.js
+++ config/connection.js
@@ -3,20 +3,27 @@ require("dotenv").config();
 var mysql = require("mysql");
 
 var connection;
+
 if (process.env.JAWSDB_URL) {
-  connection = mysql.createConnection(process.env.JAWSSDB_URL);
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-  connection = mysql.createConnection(process.env.DB_URL);
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: process.env.PW,
+    database: "burgers_db",
+  });
+
+  // Make connection
+  connection.connect(function (err) {
+    if (err) {
+      console.error("error connecting: " + err.stack);
+      return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
+
+  // Export connection for our ORM to use.
+  module.exports = connection;
 }
-
-// Make connection.
-connection.connect(function (err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
-
-// Export connection for our ORM to use.
-module.exports = connection;
